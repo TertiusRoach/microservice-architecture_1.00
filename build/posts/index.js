800360169ad97d4f2e4afb1fbafffb64eb717d6c
Tertius Roach <tertius.roach@gmail.com>
2024-09-03 13:42:49 +0200

    14. Testing the Posts Service

--- build/posts/index.js
+++ build/posts/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/posts', (req, res) => {});
+const posts = {};
 
-app.post('/posts', (req, res) => {});
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  posts[id] = {
+    id,
+    title,
+  };
+  res.status(201).send(posts[id]);
+});
 
 let port = 8080;
 app.listen(port, () => {
-  console.log(`Listening on Port: ${port}`);
+  console.log(`Listening on http://localhost:${port}/posts`);
 });
