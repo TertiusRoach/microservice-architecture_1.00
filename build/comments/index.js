4293c0bbb25b0d011a98aa92a39d7af7bee5dce5
Tertius Roach <tertius.roach@gmail.com>
2024-09-04 11:28:52 +0200

    Weird how the code of the person experiencing the problem works. I still have a lot to learn it seems.

--- build/comments/index.js
+++ build/comments/index.js
@@ -8,18 +8,20 @@ app.use(bodyParser.json());
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-  res.send(commentsByPostId[req.params.id] || []);
+  res.send(commentsByPostId[req.params.id]) || [];
 });
 
 app.post('/posts/:id/comments', (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
+
   const comments = commentsByPostId[req.params.id] || [];
 
   comments.push({ id: commentId, content });
+
   commentsByPostId[req.params.id] = comments;
-  res.status(201).send;
-  res.status(201).send({ message: 'Comment created successfully!' }); // Send a success message
+
+  res.status(201).send(comments);
 });
 
 let port = 4001;
